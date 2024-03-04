fa048ae6bffe7cccff9904d0695b9c56d7fc6c35
Mel Avina <melissaavina.ma@gmail.com>
2024-03-04 12:49:45 -0800

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -21,60 +21,56 @@ const Projects = () => {
 
 				<div className="mb-6">
 					<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-<<<<<<< HEAD
-	<div className="flex flex-col items-ßcenter"> {/* Flex container to align items horizontally */}
-=======
 						<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
->>>>>>> pages
-			<h3 className="text-xl font-bold mb-2">Breast Cancer Classification</h3>
-			<span className="text-5xl mb-2 mx-2" role="img" aria-label="Hospital">🏥</span>
-			<h3 className="text-xl font-bold mb-2 basis-1/4">Fall 2023</h3>
-		</div>
-	</a>
+							<h3 className="text-xl font-bold mb-2">Breast Cancer Classification</h3>
+							<span className="text-5xl mb-2 mx-2" role="img" aria-label="Hospital">🏥</span>
+							<h3 className="text-xl font-bold mb-2 basis-1/4">Fall 2023</h3>
+						</div>
+					</a>
 				</div >
 
-	<div className="mb-6">
-		<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-			<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
-				<h3 className="text-xl font-bold mb-2">WNBA Prediction</h3>
-				<span className="text-5xl mb-2 mx-2" role="img" aria-label="Basketball">🏀</span>
-				<h3 className="text-xl font-bold mb-2 basis-1/4">Fall 2023</h3>
-			</div>
-		</a>
-	</div>
+				<div className="mb-6">
+					<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+						<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
+							<h3 className="text-xl font-bold mb-2">WNBA Prediction</h3>
+							<span className="text-5xl mb-2 mx-2" role="img" aria-label="Basketball">🏀</span>
+							<h3 className="text-xl font-bold mb-2 basis-1/4">Fall 2023</h3>
+						</div>
+					</a>
+				</div>
 			</div >
 
-	<div className="flex flex-row justify-evenly">
-		<div className="mb-6">
-			<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-				<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
-					<h3 className="text-xl font-bold mb-2">Network Anamoly Detector</h3>
-					<span className="text-5xl mb-2 mx-2" role="img" aria-label="Credit Card">💳</span>
-					<h3 className="text-xl font-bold mb-2 basis-1/4">Summer 2023</h3>
+			<div className="flex flex-row justify-evenly">
+				<div className="mb-6">
+					<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+						<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
+							<h3 className="text-xl font-bold mb-2">Network Anamoly Detector</h3>
+							<span className="text-5xl mb-2 mx-2" role="img" aria-label="Credit Card">💳</span>
+							<h3 className="text-xl font-bold mb-2 basis-1/4">Summer 2023</h3>
+						</div>
+					</a>
 				</div>
-			</a>
-		</div>
 
-		<div className="mb-6">
-			<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-				<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
-					<h3 className="text-xl font-bold mb-2">Coursewise</h3>
-					<span className="text-5xl mb-2 mx-2" role="img" aria-label="Blue Book">📘</span>
-					<h3 className="text-xl font-bold mb-2 basis-1/4">Spring 2023</h3>
+				<div className="mb-6">
+					<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+						<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
+							<h3 className="text-xl font-bold mb-2">Coursewise</h3>
+							<span className="text-5xl mb-2 mx-2" role="img" aria-label="Blue Book">📘</span>
+							<h3 className="text-xl font-bold mb-2 basis-1/4">Spring 2023</h3>
+						</div>
+					</a>
 				</div>
-			</a>
-		</div>
 
-		<div className="mb-6">
-			<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-				<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
-					<h3 className="text-xl font-bold mb-2">Visualizing Society</h3>
-					<span className="text-5xl mb-2 mx-2" role="img" aria-label="Round Pushpin">📍</span>
-					<h3 className="text-xl font-bold mb-2 basis-1/4">Fall 2022</h3>
+				<div className="mb-6">
+					<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+						<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
+							<h3 className="text-xl font-bold mb-2">Visualizing Society</h3>
+							<span className="text-5xl mb-2 mx-2" role="img" aria-label="Round Pushpin">📍</span>
+							<h3 className="text-xl font-bold mb-2 basis-1/4">Fall 2022</h3>
+						</div>
+					</a>
 				</div>
-			</a>
-		</div>
-	</div>
+			</div>
 
 
 
