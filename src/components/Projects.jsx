6849ce527b5e00b66b9ff7e73b857f949e337846
Mel Avina <melissaavina.ma@gmail.com>
2024-03-04 22:03:27 -0800

    margin decreased

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,8 +6,7 @@ const Projects = () => {
 		<div id="projects" className="container mx-auto p-4 max-w-5xl">
 			<h2 class="text-3xl p-4 text-center font-bold from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r 
 				bg-clip-text text-transparent">projects</h2>
-			<div className="mb-8 p-6 mx-auto my-20 rounded-xl bg-white border border-gray-200 shadow">
-
+			<div className="mb-8 p-6 mx-auto rounded-xl bg-white border border-gray-200 shadow">
 				<div className="flex flex-row justify-evenly">
 					<div className="mb-6">
 						<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
@@ -73,6 +72,8 @@ const Projects = () => {
 				</div>
 			</div>
 		</div >
+
+
 	);
 }
 
