882623c43b2f1a292fd4004cd04eeaec74b87167
Mel Avina <melissaavina.ma@gmail.com>
2024-03-04 20:25:02 -0800

    test title

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,9 +2,9 @@
 const Education = () => {
 	return (
 		<div id="education" className="container mx-auto p-4 max-w-5xl">
-			<div className="mb-8 p-6 mx-auto my-20 rounded-xl bg-white border border-gray-200 shadow">
-				<h2 class="text-3xl p-4 text-center font-bold from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r 
+			<h2 class="text-3xl p-4 text-center font-bold from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r 
 				bg-clip-text text-transparent">education</h2>
+			<div className="mb-8 p-6 mx-auto my-20 rounded-xl bg-white border border-gray-200 shadow">
 				<div className="mb-6 flex justify-between items-start">
 					<div className="text-left">
 						<h3 className="text-xl font-bold mb-2">UNIVERSITY OF CALIFORNIA, DAVIS</h3>
