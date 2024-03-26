0800f6a5adc42b47ef37208ab7895cb319fb2471
Mel Avina <melissaavina.ma@gmail.com>
2024-03-25 21:36:04 -0700

    update education page
    updated cards and structure

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,13 +1,40 @@
 
 const Education = () => {
 	return (
-		<div id="education" className="container mx-auto p-4 max-w-5xl ">
+		<div id="education" className="container mx-auto p-4 max-w-5xl">
 			<h2 class="text-3xl p-4 text-center font-bold from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r 
 				bg-clip-text text-transparent">education</h2>
-			<div className="mb-4 p-6 mx-auto">
 
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-4xl mt-8">
+				<div class="md:flex">
+					<div class="md:shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="./img/Mrak3.jpg" />
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-md text-indigo-500 font-semibold uppercase">University of California, Davis</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">December 2023</a>
+						<p class="mt-2 mb-2 text-slate-500">B.S. in Applied Mathematics</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-4xl mt-8">
+				<div class="md:flex">
+					<div class="md:shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="./img/smc.jpg" />
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-md text-indigo-500 font-semibold uppercase">Santa Monica College</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">June 2019</a>
+						<p class="mt-2 mb-2 text-slate-500">A.S. in General Science</p>
+					</div>
+				</div>
+			</div>
+
+
+			<div className="mb-4 p-6 mx-auto">
 				<div className="flex flex-row">
-					<p className="text-lg mb-1 italic font-bold basis-1/3 text-left">Relevant Coursework:</p>
+					<p className="text-lg mb-1 font-bold basis-1/3 text-left">Relevant Coursework:</p>
 					<p className="text-sm text-gray-500 mb-2 basis-2/3">
 						Introduction to Programming, Introduction to Data Structures, Introduction to Programming and Microcontrollers,
 						Human-Computer Interaction, Introduction to Artificial Intelligence, Theory of Computation, Visualizing Society
@@ -16,38 +43,6 @@ const Education = () => {
 					</p>
 				</div>
 			</div>
-
-			<div className="flex flex-col">
-				<div class="relative mb-8 ">
-					<img src="./img/administration-uc-davis.jpg" alt="breast cancer image" class="h-48 w-full object-cover rounded-md object-center" />
-					<div class="absolute inset-0 bg-gray-700 opacity-60 rounded-md"></div>
-					<div class="absolute inset-0 flex items-center justify-start">
-						<div class="ml-4">
-							<a href="#">
-								<h5 class="text-3xl font-bold mb-2 text-white whitespace-nowrap">UNIVERSITY OF CALIFORNIA, DAVIS</h5>
-								<h3 class="text-xl font-bold mb-2 text-white">December 2023</h3>
-								<h3 class="text-2xl font-bold mb-2 text-white">B.S. in Applied Mathematics</h3>
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="relative  mb-8 ">
-					<img src="./img/smc.jpg" alt="breast cancer image" class="h-48 w-full object-cover rounded-md object-center" />
-					<div class="absolute inset-0 bg-gray-700 opacity-60 rounded-md"></div>
-					<div class="absolute inset-0 flex items-center justify-start">
-						<div class="ml-4">
-							<a href="#">
-								<h5 class="text-3xl font-bold mb-2 text-white whitespace-nowrap">SANTA MONICA COLLEGE</h5>
-								<h3 class="text-xl font-bold mb-2 text-white">June 2019</h3>
-							</a>
-						</div>
-					</div>
-				</div>
-
-
-			</div>
-
 
 
 		</div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,9 +12,9 @@ const Projects = () => {
 					<div className="mb-6">
 						<a href="/bike" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
 							<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
-								<h3 className="text-xl font-bold mb-2">Seoul Bike Data</h3>
+								<h3 className="text-xl font-bold mb-2 lowercase">Seoul Bike Data</h3>
 								<span className="text-5xl mb-2 mx-2" role="img" aria-label="Bicycle">🚲</span>
-								<h3 className="text-xl font-bold mb-2 basis-1/4">Spring 2022</h3>
+								<h3 className="text-xl font-bold mb-2 basis-1/4 lowercase">Spring 2022</h3>
 							</div>
 						</a>
 					</div>
@@ -22,9 +22,9 @@ const Projects = () => {
 					<div className="mb-6">
 						<a href="/cancer" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
 							<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
-								<h3 className="text-xl font-bold mb-2">Breast Cancer Classification</h3>
+								<h3 className="text-xl font-bold mb-2 lowercase">Breast Cancer Classification</h3>
 								<span className="text-5xl mb-2 mx-2" role="img" aria-label="Hospital">🏥</span>
-								<h3 className="text-xl font-bold mb-2 basis-1/4">Fall 2023</h3>
+								<h3 className="text-xl font-bold mb-2 basis-1/4 lowercase">Fall 2023</h3>
 							</div>
 						</a>
 					</div >
@@ -32,23 +32,23 @@ const Projects = () => {
 					<div className="mb-6">
 						<a href="/wnba" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
 							<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
-								<h3 className="text-xl font-bold mb-2">WNBA Prediction</h3>
+								<h3 className="text-xl font-bold mb-2 lowercase">WNBA Prediction</h3>
 								<span className="text-5xl mb-2 mx-2" role="img" aria-label="Basketball">🏀</span>
-								<h3 className="text-xl font-bold mb-2 basis-1/4">Fall 2023</h3>
+								<h3 className="text-xl font-bold mb-2 basis-1/4 lowercase">Fall 2023</h3>
 							</div>
 						</a>
 					</div>
 				</div >
 
-				<h3 className="text-3xl font-bold mb-4 basis-1/4 text-red-500 text-center">🚧 Under Construction</h3>
+				<h3 className="text-3xl font-bold mb-4 basis-1/4 text-red-500 text-center">under construction</h3>
 
 				<div className="flex flex-row justify-evenly">
 					<div className="mb-6">
 						<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
 							<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
-								<h3 className="text-xl font-bold mb-2">Network Anamoly Detector</h3>
+								<h3 className="text-xl font-bold mb-2 lowercase">Network Anamoly Detector</h3>
 								<span className="text-5xl mb-2 mx-2" role="img" aria-label="Credit Card">💳</span>
-								<h3 className="text-xl font-bold mb-2 basis-1/4">Summer 2023</h3>
+								<h3 className="text-xl font-bold mb-2 basis-1/4 lowercase">Summer 2023</h3>
 							</div>
 						</a>
 					</div>
@@ -56,9 +56,9 @@ const Projects = () => {
 					<div className="mb-6">
 						<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
 							<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
-								<h3 className="text-xl font-bold mb-2">Coursewise</h3>
+								<h3 className="text-xl font-bold mb-2 lowercase">Coursewise</h3>
 								<span className="text-5xl mb-2 mx-2" role="img" aria-label="Blue Book">📘</span>
-								<h3 className="text-xl font-bold mb-2 basis-1/4">Spring 2023</h3>
+								<h3 className="text-xl font-bold mb-2 basis-1/4 lowercase">Spring 2023</h3>
 							</div>
 						</a>
 					</div>
@@ -66,9 +66,9 @@ const Projects = () => {
 					<div className="mb-6">
 						<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
 							<div className="flex flex-col items-center"> {/* Flex container to align items horizontally */}
-								<h3 className="text-xl font-bold mb-2">Visualizing Society</h3>
+								<h3 className="text-xl font-bold mb-2 lowercase">Visualizing Society</h3>
 								<span className="text-5xl mb-2 mx-2" role="img" aria-label="Round Pushpin">📍</span>
-								<h3 className="text-xl font-bold mb-2 basis-1/4">Fall 2022</h3>
+								<h3 className="text-xl font-bold mb-2 basis-1/4 lowercase">Fall 2022</h3>
 							</div>
 						</a>
 					</div>
